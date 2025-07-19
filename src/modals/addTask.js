d654cd29b42bfe463c25111ea9bcc157699c4034
Victor Mac Laptop <175169337+itsvictoroyedeji@users.noreply.github.com>
2025-07-19 05:00:18 -0400

    Moved addTask HTML to addTask.js file

--- src/modals/addTask.js
+++ src/modals/addTask.js
@@ -1,5 +1,100 @@
 export default function addTaskModal() {
   // Add task dialog
-            
-  // End dialog
+  // <div class="form-section">
+  //   <form id="addNewTask" class="form" method="dialog">
+  //     <header class="form-header">
+  //       <div class="form-header-flex">
+  //         <div class="form-header-title-container">
+  //           <h1 class="form-header-text">
+  //             Add new task
+  //           </h1>
+  //         </div>
+  //         <div class="form-close-container">
+  //           <button class="form-close-button buttonish-elem" type="button" tabindex="0">
+  //             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path fill="currentColor" d="M5.146 5.146a.5.5 0 0 1 .708 0L12 11.293l6.146-6.147a.5.5 0 0 1 .638-.057l.07.057a.5.5 0 0 1 0 .708L12.707 12l6.147 6.146a.5.5 0 0 1 .057.638l-.057.07a.5.5 0 0 1-.708 0L12 12.707l-6.146 6.147a.5.5 0 0 1-.638.057l-.07-.057a.5.5 0 0 1 0-.708L11.293 12 5.146 5.854a.5.5 0 0 1-.057-.638z"></path></svg>
+  //           </button>
+  //         </div>
+  //       </div>
+  //     </header>
+  //     <hr class="form-header-line">
+  //     <div class="main-form">
+  //       <div class="main-form-container-flex">
+  //         <div class="form-row task-form-name">
+  //           <label for="task-name">
+  //             Name
+  //           </label>
+  //           <input type="text" name="name" id="task-name"
+  //           placeholder="Submit items by next week"
+  //           maxlength="120"
+  //           autofocus
+  //           required>
+  //         </div>
+  //         <div class="form-row task-form-description">
+  //           <label for="task-description">
+  //             Description
+  //           </label>
+  //           <input type="text" name="description" id="task-description"
+  //           max-length="240"
+  //           autofocus>
+  //         </div>
+  //         <div class="form-row task-form-date">
+  //           <label for="task-date">
+  //             Date
+  //           </label>
+  //           <input type="date" name="task-date" id="task-date"
+  //           value="2025-07-08"
+  //           min="2023-01-01"
+  //           max="2025-12-31"
+  //           autofocus>
+  //         </div>
+  //         <div class="form-row task-form-priority">
+  //           <label for="task-priority">
+  //             Priority
+  //           </label>
+  //           <select
+  //           name="task-priority" id="task-priority"
+  //           autofocus>
+  //             <option value="">Select a priority</option>
+  //             <option value="priority 1">
+  //               Priority 1
+  //             </option>
+  //             <option value="priority 2">
+  //               Priority 2
+  //             </option>
+  //             <option value="priority 3">
+  //               Priority 3
+  //             </option>
+  //             <option value="priority 4">
+  //               Priority 4
+  //             </option>
+  //           </select>
+  //         </div>
+  //         <div class="form-row task-form-project">
+  //           <label for="task-project">
+  //             Project
+  //           </label>
+  //           <select
+  //           name="task-project" id="task-project"
+  //           autofocus>
+  //             <option value="(current project)">(current project)</option>
+  //           </select>
+  //         </div>
+  //       </div>
+  //     </div>
+  //     <div class="form-buttons-container">
+  //       <hr class="form-buttons-line">
+  //       <footer class="form-buttons-flex">
+  //         <div class="form-buttons-flex-2">
+  //           <button type="button" class="button cancel-button buttonish-elem" value="cancel" formmethod="dialog" formnovalidate>
+  //             Cancel
+  //           </button>
+  //           <button type="submit" class="button submit-button buttonish-elem">
+  //             Add
+  //           </button>
+  //         </div>
+  //       </footer>
+  //     </div>
+  //   </form>
+  // </div>          
+  // // End dialog
 }
