aa1f67fcea03310e190e3caa06f5b0fd206c4759
Victor Mac Laptop <175169337+itsvictoroyedeji@users.noreply.github.com>
2025-07-25 00:37:49 -0400

    fix: Moved attachEventListeners() to own .js file (with other changes)

--- src/todoapp.js
+++ src/todoapp.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 import { greeting } from "./greetings"; // remove this line and src/greetings.js file
 import ProjectTasks from "./pages/projectTasks";
+import { attachEventListeners } from "./helpers/attachEventListeners";
 import AddTaskModal from "./modals/addTask";
 import User from "./modules/user";
 import Project from "./modules/project";
@@ -16,11 +17,10 @@ let defaultUser;
 // Initialize default user name and project if empty
 if (defaultUser === undefined) {
   defaultUser = new User({
-  name: "Victor",
-  projects: [
-    new Project({
-  })
-  ]
+    name: "Victor",
+    projects: [
+      new Project({})
+    ]
   });
 
   defaultUser.projects[0].addTodo(
@@ -31,7 +31,6 @@ if (defaultUser === undefined) {
       priority: "Priority 4"
     })
   )
-  
 };
 
 // Display Default Project Task + Add Task Modal's HTML + Event Listeners
@@ -41,124 +40,3 @@ function initLoader() {
   attachEventListeners();
   // console.log(defaultUser);
 };
-
-function attachEventListeners() {
-  const addTaskButton = document.querySelector('.add-task-button');
-  const editTaskButton = document.getElementsByClassName('task-list-edit-button');
-  const deleteTaskButton = document.getElementsByClassName('task-list-delete-button');
-  const mainDialog = document.querySelector('#addTaskDialog');
-  const submitButton = document.querySelector('.submit-button');
-  const cancelButton = document.querySelector('.cancel-button');
-  const formCloseButton = document.querySelector('.form-close-button');
-  const formTaskName = document.querySelector('#task-name');
-  const formTaskDescription = document.querySelector('#task-description');
-  const formTaskDueDate = document.querySelector('#task-date');
-  const formTaskPriority = document.querySelector('#task-priority');
-  const addText = "Add";
-  const saveText = "Save";
-  let taskItemIndex;
-  let taskItem;
-
-  // + Add Task button to popup dialog
-  addTaskButton.addEventListener("click", (e) => {
-    mainDialog.showModal();
-    submitButton.textContent = addText;
-    }
-  );
-
-  Array.from(editTaskButton).forEach((button) => 
-    button.addEventListener("click", (e) => {
-      mainDialog.showModal();
-      taskItemIndex = e.target.closest("li").dataset.index;
-      // const projectItem = document.querySelector(`.project-item[data-index="${taskItemIndex}"]`);
-      taskItem = defaultUser.projects[0].getTodos[taskItemIndex];
-      formTaskName.value = taskItem.title;
-      formTaskDescription.value = taskItem.description;
-      formTaskDueDate.value = taskItem.dueDate;
-      formTaskPriority.value = taskItem.priority;
-
-      if (formTaskPriority.value === "Priority 1") {
-        formTaskPriority.style.borderColor = "red";
-      } else if (formTaskPriority.value === "Priority 2") {
-        formTaskPriority.style.borderColor = "green";
-      } else if (formTaskPriority.value === "Priority 3") {
-        formTaskPriority.style.borderColor = "blue";
-      } else if (formTaskPriority.value === "Priority 4") {
-        formTaskPriority.style.borderColor = "#e6e6e6";
-      }
-      // Change submit button
-      submitButton.textContent = saveText;
-    }
-  ));
-
-  Array.from(deleteTaskButton).forEach((button) => 
-    button.addEventListener("click", (e) => {
-      if (confirm("Are you sure you want to delete this task?")) {;
-        taskItemIndex = e.target.closest("li").dataset.index;
-        defaultUser.projects[0].deleteTodo(taskItemIndex);
-        console.log(defaultUser);
-        // Reload DOM
-        mainDialog.textContent = '';
-        initLoader();
-      }
-    })
-  );
-
-  cancelButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    mainDialog.close();
-    formTaskName.value = "";
-    formTaskDescription.value = "";
-    formTaskDueDate.value = "";
-    formTaskPriority.priority = "";
-  });
-
-  formCloseButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    mainDialog.close();
-    formTaskName.value = "";
-    formTaskDescription.value = "";
-    formTaskDueDate.value = "";
-    formTaskPriority.priority = "";
-  });
-
-  submitButton.addEventListener("click", taskSubmit);
-
-  // Add New or Edit Task Data to User's Project
-  function taskSubmit(e) {
-    e.preventDefault();
-
-    if (formTaskName.value.length > 0) {
-      if (submitButton.textContent === addText) {
-        defaultUser.projects[0].addTodo(
-          TodoFactory ({
-            title: formTaskName.value,
-            description: formTaskDescription.value,
-            dueDate: formTaskDueDate.value,
-            priority: formTaskPriority.value
-          })
-        );
-      } else if (submitButton.textContent === saveText) {
-        defaultUser.projects[0].editTodo(
-          taskItemIndex,
-          TodoFactory ({
-            title: formTaskName.value,
-            description: formTaskDescription.value,
-            dueDate: formTaskDueDate.value,
-            priority: formTaskPriority.value
-          })
-        )
-      }
-      // Reload updated DOM and event listeners 
-      mainDialog.textContent = '';
-      initLoader();
-    } else {
-      window.alert("Task name is required");
-      return false;
-    };
-    mainDialog.close();
-  };
-}
-
-
-
