2d57612a55851cde24e77b8a2c3714bebc44df8a
Victor Mac Laptop <175169337+itsvictoroyedeji@users.noreply.github.com>
2025-08-06 20:38:33 -0400

    fix: Update localStorage when checkbox is clicked

--- src/todoapp.js
+++ src/todoapp.js
@@ -473,6 +473,7 @@ export function attachEventListeners() {
       defaultUser.projects[activeProjectIndex].deleteTodo(taskItemIndex);
       // Reload DOM
       clearModalTextContents();
+      populateStorage();
       initLoader(activeProjectIndex);
     })
   )
